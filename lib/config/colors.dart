be8ab1b6676bfd7785fae3242d7a932562eae5a6
mihir_072 <you@example.com>
2024-02-20 21:30:09 +0530

    Page Completet

---
+++ lib/config/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color lightBg = Color(0xFFD1D9E6);
+const Color blueColor = Color(0xFF246AFE);
+const Color darkBgColor = Color(0xFF242424);
+const Color lightTextColor = Color(0xFF242424);
+const Color Color1 = Color(0xFF808000);
+const Color Color3 = Color(0xFFbcb88a);
+const Color Color4 = Color(0xFF828CC4);
+const Color btn = Color(0xFF3C4DE0);
+const Color txt = Color(0xFFBCD7CC);
---
+++ lib/pages/MyButton.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:login_pages/config/colors.dart';
+
+class MyButton extends StatelessWidget {
+  final IconData icon;
+  final String Btname;
+  final VoidCallback ontap;
+  const MyButton(
+      {super.key,
+      required this.icon,
+      required this.Btname,
+      required this.ontap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ontap,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: btn,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              Btname,
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
